72dcf96a40569c9ee50c9d35e6bee59679d47a64
Declan O'Gorman <dogorman@outlook.com>
2023-12-09 11:02:06 +0000

    Day 9

--- 9/code.py
+++ 9/code.py
@@ -5,18 +5,14 @@ def getDelta(seq) :
     d = [seq[i+1] - seq[i] for i in range(len(seq)-1)]
     return [d] if d.count(0) == len(d) else  getDelta(d) + [d]
 
-def extend(seq) :
+def extend(seq, left) :
     for i in range(1, len(seq)) :
-        seq[i].append(seq[i][-1] + seq[i-1][-1])
+        if not left: seq[i].append(seq[i][-1] + seq[i-1][-1])
+        else: seq[i].insert(0,seq[i][0] - seq[i-1][0])
     return seq
 
-def extendLeft(seq) :
-    for i in range(1, len(seq)) :
-        seq[i].insert(0, seq[i][0] - seq[i-1][0])
-    return seq
-
-endSeqs = [extend(getDelta(a) + [a])[-1][-1] for a in input]
+endSeqs = [extend(getDelta(a) + [a], False)[-1][-1] for a in input]
 print(f'Part A: sum of predicted values {sum(endSeqs)}') #test assert = 18,28,68 / 114
 
-startSeqs = [extendLeft(getDelta(a) + [a])[-1][0] for a in input]
+startSeqs = [extend(getDelta(a) + [a], True)[-1][0] for a in input]
 print(f'Part B: sum of preceeding values {sum(startSeqs)}') #test assert = 5,-3,0 / 2
